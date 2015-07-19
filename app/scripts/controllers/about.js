7bbad65cdf28f1d21e7a3ec986f81f8178d7c9a4
fhuaulme <fhuaulme@talend.com>
2015-07-19 15:02:16 +0200

    Initial commit from Yeoman AngularJS generator.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lebruitdurougeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lebruitdurougeApp
+ */
+angular.module('lebruitdurougeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lebruitdurougeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lebruitdurougeApp
+ */
+angular.module('lebruitdurougeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
